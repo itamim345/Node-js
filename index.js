154a4e058008e5ca6f376e95035ddebf58094170
AF Tamim <itamim345@gmail.com>
2022-11-13 15:05:41 +0530

    POST method practice

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 4000;
 
 app.use(cors())
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Hey! I'm node one! we are done for now:)")
@@ -19,6 +20,17 @@ app.get('/users', (req, res) => {
     }
     
 })
+
+//app method
+app.post('/users', (req,res) => {
+    const newUser = req.body;
+    newUser.id = users.length;
+    users.push(newUser)
+    console.log('hitting',req.body);
+    //res.send(JSON.stringify(newUser))
+    res.json(newUser)
+})
+
 //Practicing Dynamic api
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
@@ -35,10 +47,10 @@ app.get('/fruits/banana/singpuri', (req,res) => {
 })
 
 const users = [
-    {id: 1, name: "Anurap", age: 37, email: "anurap@gmail.com"},
-    {id: 2, name: "susant", age: 22, email: "susant@gmail.com"},
-    {id: 3, name: "anil", age: 17, email: "anil@gmail.com"},
-    {id: 4, name: "rahul", age: 32, email: "rahul@gmail.com"},
+    {id: 0, name: "Anurap", age: 37, email: "anurap@gmail.com"},
+    {id: 1, name: "susant", age: 22, email: "susant@gmail.com"},
+    {id: 2, name: "anil", age: 17, email: "anil@gmail.com"},
+    {id: 3, name: "rahul", age: 32, email: "rahul@gmail.com"},
 ]
 
 app.listen(port, () => {
